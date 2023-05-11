bca58b78f23536585f7494a8745bfc60c88128b2
AdonnaiG <adonnaigirma@gmail.com>
2023-05-11 10:17:35 -0400

    fix error messages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import { MailchimpForm } from "./MailchimpForm";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/github-mark-white.png";
 
@@ -12,8 +11,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/adonnaigirma/"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://github.com/AdonnaiG"><img src={navIcon2} alt="Icon" /></a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
